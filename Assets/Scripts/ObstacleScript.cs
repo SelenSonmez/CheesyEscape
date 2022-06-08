8d8b38a23b65631524518c00b1e9b953f8eb7b94
Selen Sönmez <selensonmez2001@gmail.com>
2022-06-09 01:26:30 +0300

    update

--- Assets/Scripts/ObstacleScript.cs
+++ Assets/Scripts/ObstacleScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,10 @@ using UnityEngine;
 public class ObstacleScript : MonoBehaviour
 {
     private Animator animator;
-    
+    bool isAnimated = false;
+    public GameObject cat;
+    public Transform startPositon;
+
     public void Awake()
     {
         animator = GetComponent<Animator>();
@@ -25,11 +29,16 @@ public class ObstacleScript : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Player"))
         {
+            if (isAnimated)
+                return;
+            isAnimated = true;
             SoundManager.PlaySound("obstacle");
             StartCoroutine(Animate());
+            //GameObject catIns = Instantiate(cat, transform.position + new Vector3(0, GameObject.FindGameObjectWithTag("Player").transform.position.x, 0), transform.rotation);
         }
         
     }
+
     IEnumerator Animate()
     {    
         animator.SetTrigger("isHit");     
